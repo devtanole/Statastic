280a69902de842a61b81ed528ca98f79261e8d2f
Devin Olesen <devintanielu@gmail.com>
2026-01-31 01:34:24 +0000

    write logic for finishes and record increments

--- client/src/pages/FighterPage.tsx
+++ client/src/pages/FighterPage.tsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom';
-import { Fighter, readFighter } from '../lib/data';
+import { Fighter, readFighter, readFights, Fight } from '../lib/data';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export function FighterPage() {
   const { fighterId } = useParams();
   const [fighter, setFighter] = useState<Fighter | null>(null);
+  const [fights, setFights] = useState<Fight[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<unknown>();
 
@@ -15,6 +16,9 @@ export function FighterPage() {
       try {
         const fighter = await readFighter(Number(fighterId));
         setFighter(fighter);
+
+        const fights = await readFights(Number(fighterId));
+        setFights(fights);
       } catch (err) {
         setError(err);
       } finally {
@@ -28,18 +32,87 @@ export function FighterPage() {
   if (error) return <div>Error loading fighter</div>;
   if (!fighter) return null;
 
+  let wins = 0;
+  let losses = 0;
+  let draws = 0;
+  let finishes = 0;
+
+  fights.forEach((fight) => {
+    switch (fight.outcome.toLowerCase()) {
+      case 'w':
+      case 'win':
+      case 'W':
+      case 'WIN':
+        wins++;
+        // count finishes
+        if (fight.method) {
+          switch (fight.method.toLowerCase()) {
+            case 'ko':
+            case 'tko':
+            case 'KO':
+            case 'TKO':
+              finishes++;
+              break;
+          }
+        }
+        break;
+      case 'l':
+      case 'loss':
+      case 'LOSS':
+      case 'L':
+        losses++;
+        break;
+      case 'd':
+      case 'draw':
+      case 'nc':
+      case 'D':
+      case 'NC':
+      case 'DRAW':
+      case 't':
+      case 'T':
+      case 'TIE':
+      case 'tie':
+        draws++;
+        break;
+    }
+  });
+
   return (
     <div className="container">
       <h1>
         {fighter.firstName} {fighter.lastName}
       </h1>
       <Link to={`/fighters/${fighterId}/edit`}>Edit</Link>
-      <div className="stats">
-        <p>DOB: {fighter.dob.slice(0, 10)}</p>
-        <p>Finishes: {fighter.finishes ?? 0}</p>
-        <p>Weight Misses: {fighter.weightMisses ?? 0}</p>
-        <p>Pull Outs: {fighter.pullOuts ?? 0}</p>
-      </div>
+      <section className="fighter-summary">
+        <p>
+          <strong>Record:</strong> {wins}–{losses}
+          {draws > 0 && `–${draws}`}
+        </p>
+        <p>
+          <strong>Recorded Finishes:</strong> {finishes}
+        </p>
+
+        <p>
+          <strong>DOB:</strong> {fighter.dob.slice(0, 10)}
+        </p>
+
+        <p>
+          <strong>Finishes:</strong> {fighter.finishes ?? 0}
+        </p>
+
+        <p>
+          <strong>Weight Misses:</strong> {fighter.weightMisses ?? 0}
+        </p>
+
+        <p>
+          <strong>Pull Outs:</strong> {fighter.pullOuts ?? 0}
+        </p>
+      </section>
+
+      {/* <nav className="fighter-nav">
+        <Link to="fights">Fight History</Link>
+        <Link to="measurements">Measurements</Link>
+      </nav> */}
     </div>
   );
 }
